48f427b8a4d907fa405e9080547f03e20a120680
hirokiKKK <k.hiroki0312@gmail.com>
2025-07-01 22:41:06 +0900

    課題提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,4 +2,6 @@ let bodyWeight = 68;
 
 let bodyHeight = 1.7;
 
-console.log(bodyWeight / (bodyHeight * bodyHeight));
+let bodyMassIndex = bodyWeight / (bodyHeight * bodyHeight);
+
+console.log(bodyMassIndex);
